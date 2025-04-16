13f6eec36ec3e3bc5c78a395f0c685e5447aff11
Hillel Arki <arki.hillel@gmail.com>
2025-04-16 16:44:07 +0200

    fix: adds cors to support modern browsers

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./models');
 
 const app = express();
 
+// Mitigates the CORS Missing Allow Origin error in modern browsers
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/static`));
 
